aa9e10c9bd0c686b86cc649e5c12162c7680c1d6
Noussai24 <noussaiba.ayadi@gmail.com>
2024-04-15 15:42:16 +0200

    📦 NEW:

---
+++ exo_scrapping_decoplante.py
@@ -0,0 +1,17 @@
+#pip instal bs4
+import requests
+from bs4 import BeautifulSoup
+url = "https://decoplante.fr"
+
+response = requests.get(url)
+# print (response)
+content = response.content
+print (content)
+
+soup = BeautifulSoup (content,'html')
+img = soup.find('img')
+#print(img)
+
+
+git init
+
