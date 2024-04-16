b75f9bca15f5d7536eb8bd0a89641dc59b65b145
Noussai24 <noussaiba.ayadi@gmail.com>
2024-04-16 12:18:26 +0200

    📦 NEW:

--- wikipedia.py
+++ wikipedia.py
@@ -1,17 +1,34 @@
+
+# J'importe le module 'requests' de la bibliothèque 'requests' pour envoyer des requêtes HTTP et récupérer le contenu des pages web.
+# J'importe BeautifulSoup du module 'bs4' de la bibliothèque 'BeautifulSoup' pour analyser et extraire des données à partir du contenu HTML.
 import requests
 from bs4 import BeautifulSoup
 
-# URL de la page Wikipedia sur la chimie
+# Je déclare l'URL de la page Wikipedia sur la chimie que je souhaite analyser.
 url = "https://fr.wikipedia.org/wiki/Chimie"
 
-# Faire une requête pour obtenir le contenu de la page
+# Je fais une requête HTTP à l'URL spécifiée en utilisant la fonction 'get' du module 'requests' pour obtenir le contenu de la page.
 response = requests.get(url)
 content = response.content
 
-# Utiliser BeautifulSoup pour analyser le contenu HTML de la page
-#soup = BeautifulSoup(content, 'html.parser')
+# J'utilise BeautifulSoup pour analyser le contenu HTML de la page en créant un objet BeautifulSoup à partir du contenu HTML récupéré.
+# BeautifulSoup utilise le parseur HTML intégré 'html.parser' pour analyser le contenu et le transformer en une structure de données exploitable.
+soup = BeautifulSoup(content, 'html.parser')
 
-soup = BeautifulSoup (content,'html.parser')
+# Je recherche la première balise <img> dans le contenu HTML de la page analysée.
 img = soup.find('img')
+
+# Je printe la balise <img> trouvée dans la console.
 print(img)
+
+
+
+# Trouver la première balise <img> sur la page
+# Recherche de la première balise <img> dans le contenu HTML de la page analysée.
+img = soup.find('img')
+
+# Afficher la balise <img> trouvée
+# Affichage de la balise <img> trouvée dans la console.
+print(img)
+
 
